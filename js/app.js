6ff1896df350d7caec5bf11cc241ba0b9c682f4c
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-10 17:58:47 +0900

    drag

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 const circle = document.querySelector(".circle");
 
+//while you press left mouse button, activate 'onmosuedown'
 circle.addEventListener("mousedown", onmousedown);
+
 
 function onmousedown(e) {
 
+    //////////////////////////////////////////////////////////////
+    // accurate click position when you click on '.circle' ///////
     let shiftX = e.clientX - circle.getBoundingClientRect().left;
     let shiftY = e.clientY - circle.getBoundingClientRect().top;
 
     window.addEventListener("mousemove", onMouseMove);
-
+    // '.circle' follow on mouse //
     function onMouseMove(e) {
-        circle.style.left = e.pageX - shiftX   + "px";
+        circle.style.left = e.pageX - shiftX + "px";
         circle.style.top = e.pageY - shiftY + "px";
     }
+    //////////////////////////////////////////////////////////////
 
     // circle.addEventListener("mouseup", ()=> {
 
